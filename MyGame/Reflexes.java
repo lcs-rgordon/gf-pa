1e7cc424b074686cbb851ffb39a454f8ea0729c6
Russell Gordon <rgordon@lcs.on.ca>
2019-03-06 14:14:24 -0500

    Shows hint and waits for game to start

--- MyGame/Reflexes.java
+++ MyGame/Reflexes.java
@@ -8,6 +8,10 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Reflexes extends World
 {
+    /**
+     * Instance variables (fields a.k.a. properties)
+     */
+    private boolean shouldBeShowingHint;
 
     /**
      * Constructor for objects of class Reflexes.
@@ -15,10 +19,59 @@ public class Reflexes extends World
      */
     public Reflexes()
     {    
-        super(400, 400, 1); 
+        super(500, 400, 1); 
 
         // Make background black
         this.drawBlackBackground();
+
+        // Game starts showing hint about how to play
+        shouldBeShowingHint = true;
+    }
+
+    /**
+     * Update the action in the world
+     */
+    public void act()
+    {
+        if (shouldBeShowingHint)
+        {
+            showHint();
+            lookForGameStart();
+        }
+    }
+
+    /**
+     * Show a hint about how to play.
+     */
+    private void showHint()
+    {
+        this.showText("Welcome to Reflexes!", 250, 75);
+        this.showText("Click as many white circles as you can!", 250, 175);
+        this.showText("Don't click the red circles.", 250, 225);
+        this.showText("Press SPACE BAR to begin.", 250, 325);
+    }
+    
+    /**
+     * Hide the hint
+     */
+    private void hideHint()
+    {
+        this.showText("", 250, 75);
+        this.showText("", 250, 175);
+        this.showText("", 250, 225);
+        this.showText("", 250, 325);
+    }
+
+    /**
+     * Start the game if the user wants to.
+     */
+    private void lookForGameStart()
+    {
+        if (Greenfoot.isKeyDown("space"))
+        {
+            this.shouldBeShowingHint = false;
+            this.hideHint();
+        }
     }
     
     /**
@@ -35,7 +88,6 @@ public class Reflexes extends World
 
         // Set the new background
         this.setBackground(background);
-
     }
 
 }
