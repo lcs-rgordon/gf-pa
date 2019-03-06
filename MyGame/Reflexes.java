5eed788fcb84b6b39c0ff9ae9ed8341d431ae88a
Russell Gordon <rgordon@lcs.on.ca>
2019-03-06 09:20:42 -0500

    Changed name of World subclass

--- MyGame/Reflexes.java
+++ MyGame/Reflexes.java
@@ -1,19 +1,19 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class MyWorld here.
+ * Write a description of class Reflexes here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author R. Gordon
+ * @version March 6, 2019
  */
-public class MyWorld extends World
+public class Reflexes extends World
 {
 
     /**
-     * Constructor for objects of class MyWorld.
+     * Constructor for objects of class Reflexes.
      * 
      */
-    public MyWorld()
+    public Reflexes()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
