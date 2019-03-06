b659eeb40840e323c6557e577a021f066bb4c6b6
Russell Gordon <rgordon@lcs.on.ca>
2019-03-06 14:00:28 -0500

    Made background black

--- MyGame/Reflexes.java
+++ MyGame/Reflexes.java
@@ -16,5 +16,26 @@ public class Reflexes extends World
     public Reflexes()
     {    
         super(400, 400, 1); 
+
+        // Make background black
+        this.drawBlackBackground();
     }
+    
+    /**
+     * Set background to pure black.
+     */
+    private void drawBlackBackground()
+    {
+        // Make a new background image (transparent pixels)
+        GreenfootImage background = new GreenfootImage(400, 400);
+
+        // Draw a black rectangle over background
+        background.setColor(Color.BLACK);
+        background.fill();
+
+        // Set the new background
+        this.setBackground(background);
+
+    }
+
 }
