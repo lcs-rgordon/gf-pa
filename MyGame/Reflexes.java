8374b2419e8b4cfabff8c940b63395ef7d77cc5f
Russell Gordon <rgordon@lcs.on.ca>
2019-03-08 15:11:03 -0500

    Tidied up comments

--- MyGame/Reflexes.java
+++ MyGame/Reflexes.java
@@ -1,7 +1,7 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Reflexes here.
+ * World for the Reflexes game (manages score, timekeeping, target spawning, etc)
  * 
  * @author R. Gordon
  * @version March 6, 2019
@@ -25,7 +25,6 @@ public class Reflexes extends World
 
     /**
      * Constructor for objects of class Reflexes.
-     * 
      */
     public Reflexes()
     {    
